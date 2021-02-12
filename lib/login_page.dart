29a6a4a04717e72d31f34dd0ca78799cf5979a43
unknown <rizaldi.naldian@gmail.com>
2021-02-13 02:06:15 +0700

    menambahkan fitur login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:bogor/menuutama.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -7,23 +6,159 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  double getSmallDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 2 / 3;
+  double getBigDiameter(BuildContext context) =>
+      MediaQuery.of(context).size.width * 7 / 8;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) {
-                return MenuUtama();
-              }));
-            },
-            child: Text('Login'),
-          ),
+        body: Stack(
+          children: <Widget>[
+            Positioned(
+              right: -getSmallDiameter(context) / 3,
+              top: -getSmallDiameter(context) / 3,
+              child: Container(
+                width: getSmallDiameter(context),
+                height: getSmallDiameter(context),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.orange,
+                      Colors.white,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: -getBigDiameter(context) / 4,
+              top: -getBigDiameter(context) / 4,
+              child: Container(
+                child: Center(
+                  child: Text(
+                    'Welcome',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                width: getBigDiameter(context),
+                height: getBigDiameter(context),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    colors: [Colors.orange, Colors.white],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              right: -getBigDiameter(context) / 2,
+              bottom: -getBigDiameter(context) / 2,
+              child: Container(
+                width: getBigDiameter(context),
+                height: getBigDiameter(context),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey[200],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    margin: EdgeInsets.fromLTRB(
+                        20, MediaQuery.of(context).size.height * 0.4, 20, 10),
+                    padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.email), labelText: 'Email'),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.vpn_key), labelText: 'Password'),
+                          obscureText: true,
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 20, 20),
+                      child: Text('Forget Password'),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 0, 20, 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          height: 40,
+                          child: Container(
+                            child: Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                  borderRadius: BorderRadius.circular(20),
+                                  splashColor: Colors.blue,
+                                  child: Center(
+                                    child: Text(
+                                      'Sign-In',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  )),
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              gradient: LinearGradient(
+                                colors: [Colors.white, Colors.orange],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                              ),
+                            ),
+                          ),
+                        ),
+                        FloatingActionButton(
+                          onPressed: () {},
+                          mini: true,
+                          child: Image(
+                            image: AssetImage('assets/facebook.png'),
+                          ),
+                        ),
+                        FloatingActionButton(
+                          onPressed: () {},
+                          mini: true,
+                         
+                          child: Image(
+                            image: AssetImage('assets/google.png'),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
